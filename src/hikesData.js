5ee90278ab1c6914880eef8ff3cfb0bc27747528
ryusiwoo <colkol0516@gmail.com>
2025-04-28 19:00:53 +0900

    Update hikesData.js
    이전사진 오류수정

--- src/hikesData.js
+++ src/hikesData.js
@@ -9,8 +9,8 @@ export const hikes = [
       distance: '매우 길어요',
       comment: '나? 천왕봉 다녀온 남자야!!!',
       photos: [
-        'https://i.imgur.com/1FNDKJB.jpg',
-        'https://i.imgur.com/oggB1BP.jpg',
+        'https://i.imgur.com/Ij8yUwT.jpg',
+        'https://i.imgur.com/egDHWBT.jpg',
         'https://i.imgur.com/u4AFqnN.jpg',
         'https://i.imgur.com/PdvoTAA.jpg'
     ],
@@ -26,8 +26,10 @@ export const hikes = [
       comment: '오랫만이야 교외선아~',
       photos: [
         'https://i.imgur.com/TdWmdUX.jpg',
-        'https://i.imgur.com/DYOPv4M.jpg'
+        'https://i.imgur.com/DYOPv4M.jpg',
+        'https://i.imgur.com/YEltiSf.jpg',
+        'https://i.imgur.com/6AxahPo.jpg'
       ],
-      folderUrl: 'https://naver.me/57VqlskC' // 네이버 마이박스스 폴더 링크
+      folderUrl: 'https://imgur.com/a/250411-5FM4pT9' // 네이버 마이박스스 폴더 링크
     }
   ];
